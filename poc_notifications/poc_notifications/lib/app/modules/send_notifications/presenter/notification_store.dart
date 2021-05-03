f205960abececf8af398f74c253305590316cff5
Gabriela Santos <gabriela.pereira.santos@usp.br>
2021-05-03 18:13:22 -0300

    update
    update

--- poc_notifications/poc_notifications/lib/app/modules/send_notifications/presenter/notification_store.dart
+++ poc_notifications/poc_notifications/lib/app/modules/send_notifications/presenter/notification_store.dart
@@ -4,10 +4,10 @@ import 'package:poc_notifications/app/modules/send_notifications/domain/errors/n
 import 'package:poc_notifications/app/modules/send_notifications/domain/models/notification_params.dart';
 import 'package:poc_notifications/app/modules/send_notifications/domain/usecase/send_notification_usecase.dart';
 
+import '../../../../api_one_signal.dart';
+
 class NotificationStore extends NotifierStore<NotificationsFailures, NotificationResult> {
   final SendNotificationUseCase sendNotificationsUsecase;
-  final String appId = '4b743770-c3fb-4e15-9ab2-e3052aa64e46';
-  final String token = 'batata';
 
   NotificationStore(this.sendNotificationsUsecase) : super(NotificationResultEmpty());
 
